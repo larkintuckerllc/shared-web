815edd64d7baa1fdff3f76bb548766362d1770c5
John Tucker <john@larkintuckerllc.com>
2019-11-26 15:13:31 -0500

    hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@
 import React from 'react';
 import hello, { random } from 'larkintuckerllc-example/dist/hello';
 import there from 'larkintuckerllc-example/dist/there';
+import useDelay from 'larkintuckerllc-example/dist/useDelay';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const waiting = useDelay();
   console.log(hello);
   console.log(there);
   const randomString = random();
   console.log(randomString);
+  console.log(waiting);
   return (
     <div className="App">
       <header className="App-header">
